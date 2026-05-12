7c0aa9f9b3d8fbdfefbc73bd18138008eda8ecf2
VINICIUS MOREIRA DE ANDRADE <15316243676@campusrp-lab.riopomba.ifsudestemg.edu.br>
2026-05-12 14:40:41 -0300

    organizando mane

--- 1Semestre/AEDS/materias/recursividade/teste.cpp
+++ 1Semestre/AEDS/materias/recursividade/teste.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+//NO WHILE
+/*
 void fatorial(int n){
 	int i=n;
 	int fat=1;
@@ -15,5 +17,19 @@ int main(){
 	int n;
 	cin>>n;
 	fatorial(n);
+	return 0;
+}*/
+
+int fatorial(int n){
+	if(n==0)
+		return 1;
+	else
+		return n*fatorial(n-1);
+}
+
+int main(){
+	int n;
+	cin>>n;
+	cout << fatorial(n);
 	return 0;
 }
